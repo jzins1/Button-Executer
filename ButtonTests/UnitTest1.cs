b0f50dbae10665418759df258a031d9c661cba2e
Joseph Zins <jzins1@css.edu>
2022-12-13 10:53:06 -0600

    Fixed XML Reference Error

--- ButtonTests/UnitTest1.cs
+++ ButtonTests/UnitTest1.cs
@@ -7,26 +7,25 @@ namespace ButtonTests
     [TestClass]
     public class UnitTest1
     {
-        // Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\buttons.xml")
         [TestMethod]
         public void TestLabelTooLong()
         {
             ButtonValidator buttonValidator = new ButtonValidator();
-            bool status = buttonValidator.validateButtons(Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\buttons_too_long.xml"));
+            bool status = buttonValidator.validateButtons(Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\Button Executer\\buttons_too_long.xml"));
             Assert.IsFalse(status);
         }
         [TestMethod]
         public void TestInvalidType()
         {
             ButtonValidator buttonValidator = new ButtonValidator();
-            bool status = buttonValidator.validateButtons(Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\buttons_invalid_type.xml"));
+            bool status = buttonValidator.validateButtons(Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\Button Executer\\buttons_invalid_type.xml"));
             Assert.IsFalse(status);
         }
         [TestMethod]
         public void TestInvalidLink()
         {
             ButtonValidator buttonValidator = new ButtonValidator();
-            bool status = buttonValidator.validateButtons(Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\buttons_invalid_link.xml"));
+            bool status = buttonValidator.validateButtons(Environment.ExpandEnvironmentVariables("%userprofile%\\Documents\\Button Executer\\buttons_invalid_link.xml"));
             Assert.IsFalse(status);
         }
     }
